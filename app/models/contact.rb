b6fce2d5dc1438be356a5e6ca6ecebcdd16eae6a
Pat Allan <pat@freelancing-gods.com>
2008-09-29 15:03:40 +0700

    Making name field sortable

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < ActiveRecord::Base
   belongs_to :state
   
   define_index do
-    indexes [first_name, last_name], :as => :name
+    indexes [first_name, last_name], :as => :name, :sortable => true
     indexes street_address, city, country, postal_code
     
     has state_id
