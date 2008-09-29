3d7a90b06cd6b116f30036735ae73c86c1d4393d
Pat Allan <pat@freelancing-gods.com>
2008-09-29 14:31:04 +0700

    Adding will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -65,3 +65,6 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
+
+gem 'mislav-will_paginate', '~> 2.2'
+require 'will_paginate'
