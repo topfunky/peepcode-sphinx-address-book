3a56439ce5cf041d7e8ab1fa811270bf34e77116
Pat Allan <pat@freelancing-gods.com>
2008-09-29 14:48:07 +0700

    Migration given some actual code

--- db/migrate/20080929074602_switch_states.rb
+++ db/migrate/20080929074602_switch_states.rb
@@ -1,7 +1,32 @@
 class SwitchStates < ActiveRecord::Migration
   def self.up
+    select_values(
+      "SELECT DISTINCT state FROM contacts"
+    ).each do |state|
+      State.create :name => state
+    end
+    
+    add_column :contacts, :state_id, :integer
+    add_index  :contacts, :state_id
+    
+    Contact.find(:all).each do |contact|
+      contact.update_attributes(
+        :state_id => State.find_by_name(contact.attributes["state"]).id
+      )
+    end
+    
+    remove_column :contacts, :state
   end
 
   def self.down
+    add_column :contacts, :state, :string
+    
+    Contact.find(:all).each do |contact|
+      execute "UPDATE contacts SET state = '#{contact.state.name}' WHERE id = #{contact.id}"
+    end
+    
+    remove_column :contacts, :state_id
+    
+    State.find(:all).each { |state| state.destroy }
   end
 end
