b47dc7689bcbd38454679a7e6b6153de91624187
Pat Allan <pat@freelancing-gods.com>
2008-09-29 14:47:03 +0700

    Adding state association to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  belongs_to :state
+  
   define_index do
     indexes [first_name, last_name], :as => :name
     indexes street_address, city, state, country, postal_code
